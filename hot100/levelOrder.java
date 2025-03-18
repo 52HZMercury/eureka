f510b979a20a3d4599783e2ecfe83b7b0cf9e018
52HZMercury <chenning_william@163.com>
2025-03-18 16:30:02 +0800

    feat: levelOrder

---
+++ hot100/levelOrder.java
@@ -0,0 +1,38 @@
+package hot100;
+
+import java.util.*;
+
+/**
+ * @program: code
+ * @description:
+ * @author: Mr.Mercury
+ * @create: 2025-03-18 16:28
+ **/
+public class levelOrder {
+
+    //层序遍历  记得使用的是队列
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if(root!=null){
+            queue.add(root);
+        }
+        while(!queue.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+                level.add(node.val);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+}
